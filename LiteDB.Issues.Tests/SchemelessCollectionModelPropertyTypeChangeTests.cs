8a3512e2a18cf69a53ef428d56c6f550c8573a09
Deilan <deilan@users.noreply.github.com>
2018-12-30 15:30:38 +0300

    add tests for model property's type change in a schemeless collection

---
+++ LiteDB.Issues.Tests/SchemelessCollectionModelPropertyTypeChangeTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using LiteDB.Issues.Tests.Common;
+using Xunit;
+
+namespace LiteDB.Issues.Tests
+{
+    public class SchemelessCollectionModelPropertyTypeChangeTests : IDisposable
+    {
+        private const string CustomerCollectionName = "Customer";
+        private readonly LiteDatabaseFixture _liteDatabaseFixture;
+
+        public class NullableIntCustomer
+        {
+            public int Id { get; set; }
+            public int? Sum { get; set; }
+        }
+
+        public class IntCustomer
+        {
+            public int Id { get; set; }
+            public int Sum { get; set; }
+        }
+
+        public SchemelessCollectionModelPropertyTypeChangeTests()
+        {
+            _liteDatabaseFixture = new LiteDatabaseFixture();
+        }
+
+        [Fact]
+        public void ChangeNullableIntToNullableDecimal_WithoutMapping_Succeeded()
+        {
+            // Arrange
+            var customer = new NullableIntCustomer() {Sum = 100};
+
+            var liteDatabase = _liteDatabaseFixture.Instance;
+
+            // Act
+            var customersOldCollection = liteDatabase.GetCollection(CustomerCollectionName);
+            customersOldCollection.Insert(BsonMapper.Global.ToDocument(customer));
+
+            var customersNewCollection = liteDatabase.GetCollection(CustomerCollectionName);
+            var customerNew = customersNewCollection.FindById(customer.Id);
+
+            // Assert
+            Assert.Equal(100, customerNew.RawValue["Sum"].AsDecimal);
+        }
+
+        [Fact]
+        public void ChangeIntToDecimal_WithoutMapping_Succeeded()
+        {
+            // Arrange
+            var customer = new IntCustomer() { Sum = 100 };
+
+            var liteDatabase = _liteDatabaseFixture.Instance;
+
+            // Act
+            var customersOldCollection = liteDatabase.GetCollection(CustomerCollectionName);
+            customersOldCollection.Insert(BsonMapper.Global.ToDocument(customer));
+
+            var customersNewCollection = liteDatabase.GetCollection(CustomerCollectionName);
+            var customerNew = customersNewCollection.FindById(customer.Id);
+
+            // Assert
+            Assert.Equal(100, customerNew.RawValue["Sum"].AsDecimal);
+        }
+
+        public void Dispose()
+        {
+            this._liteDatabaseFixture.Dispose();
+        }
+    }
+}
